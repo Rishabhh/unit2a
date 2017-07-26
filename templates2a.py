161916c822e5d996d344e0f26daf721407da60b7
Rishabhh <rishabhh.singh93@gmail.com>
2017-07-26 19:42:13 +0530

    jinja template basics

--- templates2a.py
+++ templates2a.py
@@ -1,14 +1,12 @@
 import os
 import webapp2
+import jinja2
 
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 form_html= """
-	<form>
-		<h2><strong>ADD FOOD </strong></h2>
-		<input type="text" name = "food" >
-		%s
-		<button>Add</button>
-	</form>
+
 """
 
 hidden_html = """
@@ -31,9 +29,19 @@ class Handler(webapp2.RequestHandler):
 	def write(self,*a,**kw):
 		self.response.out.write(*a,**kw)
 
+	def render_str(self,template,**params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self,template,**kw):
+		self.write(self.render_str(template,**kw))
+
 class MainPage(Handler):
-	def get(self):
-		output = form_html									#how it works is that initially output_hidden is empty but after we enter food for first time 
+	def get(self,name="true"):
+		name = self.request.get("name")
+		self.render("shopping_list.html",name=name)
+
+		# output = form_html									#how it works is that initially output_hidden is empty but after we enter food for first time 
 															# and the form is rendered again, url contains the parameter name="apple" and hence items will
 															# not be null this time and hence output_hidden variable is contructed and it actually is a
 															# concatenated string of strings like <input type="hidden" name="food" value= "apple">
@@ -47,24 +55,24 @@ class MainPage(Handler):
 															# CONCATENATED with form_html (output variable actually) so that form_html and shopping list 
 															# is displayed on the html page.
 
-		output_hidden=""
+		# output_hidden=""
 		
-		items = self.request.get_all("food")
-		if items:
-			output_items= ""
-			for item in items:
-				if item :
-					output_hidden +=hidden_html % item
+		# items = self.request.get_all("food")
+		# if items:
+		# 	output_items= ""
+		# 	for item in items:
+		# 		if item :
+		# 			output_hidden +=hidden_html % item
 					
-					output_items += items_html % item
+		# 			output_items += items_html % item
 
 			
-			output_shopping = shopping_list_html % output_items   # creates the shopping list's shopping_list_html string with all items
-			output += output_shopping     # concatenating the form_html html string with shopping_list_html html string
+		# 	output_shopping = shopping_list_html % output_items   # creates the shopping list's shopping_list_html string with all items
+		# 	output += output_shopping     # concatenating the form_html html string with shopping_list_html html string
 		
-		output = output % output_hidden    
+		# output = output % output_hidden    
 		
-		self.write(output)
+		# self.write(output)
 
 
 app = webapp2.WSGIApplication([
